749e2f38c55206c8a154c26abc2c5d1c8dc1fab2
peware <danielrodriguezd13@gmail.com>
2023-08-16 11:36:14 -0500

    Agregando archivos de la versión de producción

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderIndex/>
+  <BannerTraders/>
+  <ContentCards/>
+  <BannerLeading/>
+  <ComunyTraders/>
+  <FooterIndex/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderIndex from './components/header/HeaderIndex.vue'
+import BannerTraders from './components/content/BannerTraders.vue'
+import ContentCards from './components/content/ContentCards.vue'
+import BannerLeading from './components/content/BannerLeading.vue'
+import ComunyTraders from './components/content/ComunyTraders.vue'
+import FooterIndex from './components/footer/FooterIndex.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderIndex,
+    BannerTraders,
+    ContentCards,
+    BannerLeading,
+    ComunyTraders,
+    FooterIndex
   }
 }
 </script>
@@ -20,7 +34,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
